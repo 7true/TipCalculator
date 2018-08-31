a9f8e488a5258b4f2fdecbdab285f49fbe99c9cf
alx <37true@gmail.com>
2018-08-31 08:31:35 +0700

    layout

--- app/src/main/java/tk/alltrue/tipcalculator/MainActivity.java
+++ app/src/main/java/tk/alltrue/tipcalculator/MainActivity.java
@@ -2,8 +2,23 @@ package tk.alltrue.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0;
+    private double percent = 0.15;
+    private TextView 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
