3f292ac50f5b8aef525f70542988feee07e59175
Aleksandr Malakhov <7true@mail.ru>
2018-09-02 10:21:58 +0700

    completed

--- app/src/main/java/tk/alltrue/tipcalculator/MainActivity.java
+++ app/src/main/java/tk/alltrue/tipcalculator/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import java.text.NumberFormat;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
@@ -18,11 +19,76 @@ public class MainActivity extends AppCompatActivity {
 
     private double billAmount = 0.0;
     private double percent = 0.15;
-    private TextView 
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));
+
+        EditText amountEditText = (EditText)findViewById(R.id.amountEditText);
+        amountEditText.addTextChangedListener(amountEditTextWather);
+
+        SeekBar percentageSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+        percentageSeekBar.setOnSeekBarChangeListener(seekBarChangeListener);
     }
+
+    private void calculate() {
+        percentTextView.setText(percentFormat.format(percent));
+
+        double tip = billAmount * percent;
+        double total = billAmount + tip;
+
+        tipTextView.setText(currencyFormat.format(tip));
+        totalTextView.setText(currencyFormat.format(total));
+    }
+
+    private final OnSeekBarChangeListener seekBarChangeListener =
+            new OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    percent = progress / 100.0;
+                    calculate();
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) { }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) { }
+            };
+
+    private final TextWatcher amountEditTextWather = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                billAmount = Double.parseDouble(s.toString())/100.0;
+                amountTextView.setText(currencyFormat.format(billAmount));
+            }
+            catch (NumberFormatException e) {
+                amountTextView.setText("");
+                billAmount = 0.0;
+            }
+
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+    };
+
 }
